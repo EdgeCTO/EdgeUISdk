e87e1ec8d84fc3a0ab0854b1da274cb50ee81c96
zeeshan-edgevideo <zeeshan@edgevideo.com>
2023-03-22 16:43:32 +0500

    Done : added threads and methods to control thread

--- app/src/main/java/com/example/edgeuisdk/MainActivity.java
+++ app/src/main/java/com/example/edgeuisdk/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.edgesdk.EdgeSdk;
 import com.edgesdk.Ticker;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Ticker ticker = new Ticker(this);
+        EdgeSdk edgeSdk = new EdgeSdk(this);
+
+//        edgeSdk.getLocalStorageManager().storeBooleanValue(true, com.edgesdk.Utils.Constants.IS_TICKER_ALLOWED_TO_HIDE);
+//        edgeSdk.getLocalStorageManager().storeBooleanValue(false,com.edgesdk.Utils.Constants.IS_OPT_OUT_W2E_ENABLED);
+//        edgeSdk.getLocalStorageManager().storeBooleanValue(true,com.edgesdk.Utils.Constants.IS_VIEWER_WALLET_ADDRESS_FORWARDED);
+//        edgeSdk.getLocalStorageManager().storeStringValue("0x1CE5db82533E4Fec3e1D983D89070F8185fC163F",com.edgesdk.Utils.Constants.WALLET_ADDRESS);
+
+
+        edgeSdk.start();
+        edgeSdk.startStaking();
+        Ticker ticker = new Ticker(this,edgeSdk);
+
+        ticker.setBackpressed(false);
+        ticker.setPlaying(true);
+        
         LinearLayout layout = findViewById(R.id.main_layout);
         layout.addView(ticker);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                    edgeSdk.getW2EarnManager().updateBaseRateOnServer(600);
+                    Thread.sleep(10000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
